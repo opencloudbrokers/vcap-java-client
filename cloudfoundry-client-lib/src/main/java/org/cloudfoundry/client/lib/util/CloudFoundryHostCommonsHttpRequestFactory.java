765c8ae5c2a095bf34294025d2319a82037112a6
David Dawson <david.dawson@dawsonsystems.com>
2012-09-06 16:55:50 +0100

    Add method to inject names for cloud foundries into the client.  Allows heavily automated testing when cloud foundry instances are being created and destroyed and cooperation from a friendly DNS server or hosts file is not feasible or desirable

--- cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/util/CloudFoundryHostCommonsHttpRequestFactory.java
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/util/CloudFoundryHostCommonsHttpRequestFactory.java
@@ -1,11 +1,52 @@
 package org.cloudfoundry.client.lib.util;
 
-/**
- * Created with IntelliJ IDEA.
- * User: david
- * Date: 04/09/12
- * Time: 16:54
- * To change this template use File | Settings | File Templates.
+
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.URIException;
+import org.springframework.http.client.CommonsClientHttpRequestFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/*
+ * Allows selectively overriding of the names that we can refer to remote cloud foundry instances as.
+ * Eg, if a CF instance is running on 111.111.111.111, and calls itself simple.cloud, we can add an override here
+ * for simplecloud/ 111.111.111.111.  From then on, we can refer to X.simple.cloud in API calls, and that will be handled
+ * correctly.
+ *
+ * This eases automated testing concerns when updating of DNS or hosts files is not desirable.
  */
-public class CloudFoundryHostCommonsHttpRequestFactory {
+public class CloudFoundryHostCommonsHttpRequestFactory extends CommonsClientHttpRequestFactory {
+
+  private static Map<String, String> virtualHostToIP = new HashMap<String, String>();
+
+  public static void addIpToCoudFoundryNameMapping(String ipaddress, String cloudFoundryName) {
+    virtualHostToIP.put(cloudFoundryName, ipaddress);
+  }
+
+  @Override
+  public void postProcessCommonsHttpMethod(HttpMethodBase httpMethod) {
+    try {
+      String cloudFoundryName = getCloudFoundryName(httpMethod.getURI());
+      if (virtualHostToIP.get(cloudFoundryName) != null) {
+        String ip = virtualHostToIP.get(cloudFoundryName);
+        httpMethod.getParams().setVirtualHost(httpMethod.getURI().getAuthority());
+        URI uri = httpMethod.getURI();
+        uri.setEscapedAuthority(ip);
+        httpMethod.setURI(uri);
+      }
+    } catch (URIException e) {
+      e.printStackTrace();
+    }
+  }
+
+  static String getCloudFoundryName(URI uri) throws URIException {
+    String name = uri.getAuthority();
+    if (!name.contains(".")) {
+      return name;
+    }
+    return name.substring(name.indexOf(".") + 1);
+  }
 }
--- cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/util/CloudFoundryHostCommonsHttpRequestFactoryTest.java
+++ cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/util/CloudFoundryHostCommonsHttpRequestFactoryTest.java
@@ -1,11 +1,49 @@
 package org.cloudfoundry.client.lib.util;
 
-/**
- * Created with IntelliJ IDEA.
- * User: david
- * Date: 06/09/12
- * Time: 15:35
- * To change this template use File | Settings | File Templates.
- */
+import org.apache.commons.httpclient.HttpMethodBase;
+import static org.mockito.Mockito.*;
+
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.URIException;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.junit.Test;
+import static junit.framework.TestCase.*;
+
 public class CloudFoundryHostCommonsHttpRequestFactoryTest {
+
+  @Test
+  public void whenIpAddedUriIsUpdated() throws URIException {
+    HttpMethodBase method = mock(HttpMethodBase.class);
+
+    CloudFoundryHostCommonsHttpRequestFactory factory = new CloudFoundryHostCommonsHttpRequestFactory();
+    HttpMethodParams params = new HttpMethodParams();
+
+    CloudFoundryHostCommonsHttpRequestFactory.addIpToCoudFoundryNameMapping("111.111.111.111", "somecloud");
+
+    when(method.getURI()).thenReturn(new URI("http://api.somecloud/apps", false));
+    when(method.getParams()).thenReturn(params);
+
+    factory.postProcessCommonsHttpMethod(method);
+
+    verify(method).setURI(new URI("http://111.111.111.111/apps"));
+
+    assertEquals("api.somecloud", params.getVirtualHost());
+  }
+
+  @Test
+  public void whenNoCloudNameMatchDoNothing() throws URIException {
+    HttpMethodBase method = mock(HttpMethodBase.class);
+
+    CloudFoundryHostCommonsHttpRequestFactory factory = new CloudFoundryHostCommonsHttpRequestFactory();
+    HttpMethodParams params = new HttpMethodParams();
+
+    when(method.getURI()).thenReturn(new URI("http://api.somecloud/apps", false));
+    when(method.getParams()).thenReturn(params);
+
+    factory.postProcessCommonsHttpMethod(method);
+
+    verify(method, never()).setURI((URI) any());
+
+    assertEquals(null, params.getVirtualHost());
+  }
 }
