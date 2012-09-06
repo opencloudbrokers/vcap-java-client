ab6dc1bb146d43fa7aad30dfdaa8824b62b2f061
David Dawson <david.dawson@dawsonsystems.com>
2012-09-06 16:55:32 +0100

    Add method to inject names for cloud foundries into the client.  Allows heavily automated testing when cloud foundry instances are being created and destroyed and cooperation from a friendly DNS server or hosts file is not feasible or desirable

---
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/util/CloudFoundryHostCommonsHttpRequestFactory.java
@@ -0,0 +1,11 @@
+package org.cloudfoundry.client.lib.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david
+ * Date: 04/09/12
+ * Time: 16:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class CloudFoundryHostCommonsHttpRequestFactory {
+}
---
+++ cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/util/CloudFoundryHostCommonsHttpRequestFactoryTest.java
@@ -0,0 +1,11 @@
+package org.cloudfoundry.client.lib.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david
+ * Date: 06/09/12
+ * Time: 15:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class CloudFoundryHostCommonsHttpRequestFactoryTest {
+}
